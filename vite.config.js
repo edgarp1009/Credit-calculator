2b58741b11d21fa77ddeaaeb846068bbc2978a54
edgarp1009 <edgarp1009@gmail.com>
2024-08-04 09:14:38 +0200

    making the web responsive

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://edgarp1009.github.io/Credit-calculator'
 })
